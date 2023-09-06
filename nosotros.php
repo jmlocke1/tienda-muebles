552ec0c213750847b7950ae5e72a80e1ce09d79d
Josemi Xeon <worldcode.info@gmail.com>
2023-09-07 00:12:32 +0200

    Comenzando con la página de Nosotros

--- nosotros.php
+++ nosotros.php
@@ -1,10 +1,21 @@
 <?php 
-$titulo = "Home";
+$titulo = "Nosotros";
 include "templates/header.php"; 
 ?>
 
 	<main class="contenido-principal contenedor">
 		<h2 class="text-center">Sobre Nosotros</h2>
+
+		<div class="contenido-nosotros">
+			<div class="imagen">
+				<img src="img/nosotros.jpg" alt="Imagen Nosotros">
+			</div>
+			<div class="informacion-nosotros">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem adipisci rerum et vero pariatur modi ipsam omnis nemo eius quibusdam dolorem eaque, sunt similique doloremque debitis praesentium ad sit.</p>
+				<p>Earum in quidem natus qui necessitatibus quisquam sint doloribus, neque id veniam assumenda eos dolorem. Nobis dolorum aperiam dolore eveniet explicabo nemo quod in blanditiis vero sapiente ut ea fugit neque esse temporibus, animi sunt praesentium repudiandae, aspernatur beatae, ipsa quaerat obcaecati dolor? Libero vel ullam, fuga temporibus iusto dolor dolore! Quis, libero? Repellat dolor quam voluptatum, tenetur magnam necessitatibus recusandae, quo cumque consectetur, minima dolore? Sint consequuntur aut recusandae ullam sit dicta inventore nihil adipisci aliquid et sapiente, soluta porro voluptatem beatae mollitia esse impedit minus saepe qui velit dignissimos officia! Quas, officiis illum iusto, omnis tempora deserunt velit animi fuga veniam quis debitis voluptas qui nostrum tempore, sunt ut delectus dolorum ipsum optio reiciendis sed. Porro impedit unde iusto. Ipsam quos culpa facere rerum. Veniam cumque est quisquam hic ab recusandae nulla aperiam. Consequuntur, quae adipisci.</p>
+			</div>
+		</div>
+		
 	</main>
-	
+
 <?php include "templates/footer.php"; ?>
