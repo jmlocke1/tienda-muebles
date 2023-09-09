714c8f51a3ead02fad56bbfc5a989ed0c4a6faac
Josemi Xeon <worldcode.info@gmail.com>
2023-09-09 22:36:21 +0200

    Creada página de Tienda

--- tienda.php
+++ tienda.php
@@ -3,8 +3,77 @@ $titulo = "Tienda";
 include "templates/header.php"; 
 ?>
 
-	<main class="contenido-principal contenedor">
+<main class="contenido-principal contenedor">
 		<h2 class="text-center">Nuestros Productos</h2>
+
+		<div class="listado-productos"> <!-- Listado de productos -->
+			<div class="producto">
+				<img src="img/producto1.jpg" alt="Imagen Producto">
+
+				<div class="texto-producto">
+					<h3>Producto 1</h3>
+					<p>Aliquam, cum incidunt temporibus rerum suscipit esse provident dolor laborum necessitatibus nobis velit repellat dolorum, amet eligendi! Adipisci ducimus fuga facere in consequatur obcaecati deserunt possimus, ad facilis reprehenderit recusandae magni ut quis nostrum!</p>
+					<p class="precio">$1,000,000.00</p>
+
+					<a class="btn" href="#">Agregar al carrito</a>
+				</div> <!-- Info Producto -->
+			</div> <!-- Producto -->
+			<div class="producto">
+				<img src="img/producto2.jpg" alt="Imagen Producto">
+
+				<div class="texto-producto">
+					<h3>Producto 2</h3>
+					<p>Aliquam, cum incidunt temporibus rerum suscipit esse provident dolor laborum necessitatibus nobis velit repellat dolorum, amet eligendi! Adipisci ducimus fuga facere in consequatur obcaecati deserunt possimus, ad facilis reprehenderit recusandae magni ut quis nostrum!</p>
+					<p class="precio">$1,000,000.00</p>
+
+					<a class="btn" href="#">Agregar al carrito</a>
+				</div> <!-- Info Producto -->
+			</div> <!-- Producto -->
+			<div class="producto">
+				<img src="img/producto3.jpg" alt="Imagen Producto">
+
+				<div class="texto-producto">
+					<h3>Producto 3</h3>
+					<p>Aliquam, cum incidunt temporibus rerum suscipit esse provident dolor laborum necessitatibus nobis velit repellat dolorum, amet eligendi! Adipisci ducimus fuga facere in consequatur obcaecati deserunt possimus, ad facilis reprehenderit recusandae magni ut quis nostrum!</p>
+					<p class="precio">$1,000,000.00</p>
+
+					<a class="btn" href="#">Agregar al carrito</a>
+				</div> <!-- Info Producto -->
+			</div> <!-- Producto -->
+			<div class="producto">
+				<img src="img/producto4.jpg" alt="Imagen Producto">
+
+				<div class="texto-producto">
+					<h3>Producto 4</h3>
+					<p>Aliquam, cum incidunt temporibus rerum suscipit esse provident dolor laborum necessitatibus nobis </p>
+					<p class="precio">$1,000,000.00</p>
+
+					<a class="btn" href="#">Agregar al carrito</a>
+				</div> <!-- Info Producto -->
+			</div> <!-- Producto -->
+			<div class="producto">
+				<img src="img/producto5.jpg" alt="Imagen Producto">
+
+				<div class="texto-producto">
+					<h3>Producto 5</h3>
+					<p>Aliquam, cum incidunt temporibus rerum suscipit esse provident dolor laborum necessitatibus nobis velit repellat dolorum, amet eligendi! Adipisci ducimus fuga facere in consequatur obcaecati deserunt possimus, ad facilis reprehenderit recusandae magni ut quis nostrum!</p>
+					<p class="precio">$1,000,000.00</p>
+
+					<a class="btn" href="#">Agregar al carrito</a>
+				</div> <!-- Info Producto -->
+			</div> <!-- Producto -->
+			<div class="producto">
+				<img src="img/producto6.jpg" alt="Imagen Producto">
+
+				<div class="texto-producto">
+					<h3>Producto 6</h3>
+					<p>Aliquam, cum incidunt temporibus rerum suscipit esse provident dolor laborum necessitatibus nobis velit repellat dolorum, amet eligendi! Adipisci ducimus fuga facere in consequatur obcaecati deserunt possimus, ad facilis reprehenderit recusandae magni ut quis nostrum!</p>
+					<p class="precio">$1,000,000.00</p>
+
+					<a class="btn" href="#">Agregar al carrito</a>
+				</div> <!-- Info Producto -->
+			</div> <!-- Producto -->
+		</div>	<!-- Fin de listado de productos -->
 	</main>
 	
 <?php include "templates/footer.php"; ?>
