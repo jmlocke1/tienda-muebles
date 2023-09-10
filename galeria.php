56c700f097f479370ef2422991230b183bd2cf08
Josemi Xeon <worldcode.info@gmail.com>
2023-09-10 21:29:15 +0200

    Creando la galería

--- galeria.php
+++ galeria.php
@@ -5,6 +5,53 @@ include "templates/header.php";
 
 	<main class="contenido-principal contenedor">
 		<h2 class="text-center">Galería</h2>
+
+		<ul class="galeria">
+			<li>
+				<a href="img/galeria_01.jpg">
+					<img src="img/galeria_01.jpg" alt="Imagen 01 de la Galería" title="Pulsa para ampliar la imagen 01">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_02.jpg">
+					<img src="img/galeria_02.jpg" alt="Imagen 02 de la Galería" title="Pulsa para ampliar la imagen 02">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_03.jpg">
+					<img src="img/galeria_03.jpg" alt="Imagen 03 de la Galería" title="Pulsa para ampliar la imagen 03">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_04.jpg">
+					<img src="img/galeria_04.jpg" alt="Imagen 04 de la Galería" title="Pulsa para ampliar la imagen 04">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_05.jpg">
+					<img src="img/galeria_05.jpg" alt="Imagen 05 de la Galería" title="Pulsa para ampliar la imagen 05">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_06.jpg">
+					<img src="img/galeria_06.jpg" alt="Imagen 06 de la Galería" title="Pulsa para ampliar la imagen 06">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_07.jpg">
+					<img src="img/galeria_07.jpg" alt="Imagen 07 de la Galería" title="Pulsa para ampliar la imagen 07">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_08.jpg">
+					<img src="img/galeria_08.jpg" alt="Imagen 08 de la Galería" title="Pulsa para ampliar la imagen 08">
+				</a>
+			</li>
+			<li>
+				<a href="img/galeria_09.jpg">
+					<img src="img/galeria_09.jpg" alt="Imagen 09 de la Galería" title="Pulsa para ampliar la imagen 09">
+				</a>
+			</li>
+		</ul>
 	</main>
-	
 <?php include "templates/footer.php"; ?>
